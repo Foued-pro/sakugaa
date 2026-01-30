4b5a4584740d9e23aa4f6f4e542ed2652fa2a9aa
Foued Attar <foued.attar@lyceeastier.com>
2026-01-31 00:35:59 +0100

    resolution de pb v3

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-// app/page.tsx
+const runtime = 'edge';
 import { HomeClient } from "@/components/home-client";
 
 async function getClips() {
   try {
     const res = await fetch(
         'https://www.sakugabooru.com/post.json?limit=20&page=1&tags=production_materials',
-        { cache: 'no-store' }
+        { next: { revalidate: 60 } }
     );
     if (!res.ok) return [];
     return await res.json();
@@ -17,6 +17,5 @@ async function getClips() {
 
 export default async function Home() {
   const clips = await getClips();
-  console.log("Server clips fetched:", clips.length); // Debug
   return <HomeClient initialClips={clips} />;
 }
